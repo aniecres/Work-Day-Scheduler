ad8eceae50a46b02638ffd8770ab6108712cd8d1
aniecres <111096788+aniecres@users.noreply.github.com>
2022-11-02 19:41:41 -0400

    add current date js file

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,12 @@
 // adding current day
-const date = new Date;
-console.log(date);
+// const date = new Date;
+// console.log(date);
+
+let current = moment().format("dddd, MMMM, YYYY");
+let newDate = document.getElementById("currentDay");
+newDate.innerHTML = current;
+
 
 // Blocks for the past, present, future
-$(".time-div").each(function)
+
 
