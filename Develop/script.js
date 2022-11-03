cad0831441493aa8ce14f33116d1c2d70182c9a4
aniecres <111096788+aniecres@users.noreply.github.com>
2022-11-03 17:32:20 -0400

    fixed classes

--- Develop/script.js
+++ Develop/script.js
@@ -15,7 +15,7 @@ $(".time").each(function() {
 
 if (currentDate == time) {
     $(this).addClass("present");
-    $(this).children(".time-block").addClass("present");
+    $(this).children(".timeBlock").addClass("present");
 }  else if (currentDate < time) {
     $(this).removeClass("present");
     $(this).addClass("future");
@@ -29,21 +29,21 @@ if (currentDate == time) {
 // Saving data to the local storage 
 $(".save").click(function (event) {
     event.preventDefault();
-    let saveInfo = $(this).siblings(".time-block").val();
+    let saveInfo = $(this).siblings(".timeBlock").val();
     let info = $(this).parent().attr("id").split("-")[1];
 localStorage.setItem(saveInfo, info); 
 });
 
 // Data from local starage
-$("#hour08 .time-block").val(localStorage.getItem("08"));
-$("#hour09 .time-block").val(localStorage.getItem("09"));
-$("#hour10 .time-block").val(localStorage.getItem("10"));
-$("#hour11 .time-block").val(localStorage.getItem("11"));
-$("#hour12 .time-block").val(localStorage.getItem("12"));
-$("#hour01 .time-block").val(localStorage.getItem("01"));
-$("#hour02 .time-block").val(localStorage.getItem("02"));
-$("#hour03 .time-block").val(localStorage.getItem("05"));
-$("#hour04 .time-block").val(localStorage.getItem("04"));
-$("#hour05 .time-block").val(localStorage.getItem("08"));
+$("#hour08 .timeBlock").val(localStorage.getItem("08"));
+$("#hour09 .timeBlock").val(localStorage.getItem("09"));
+$("#hour10 .timeBlock").val(localStorage.getItem("10"));
+$("#hour11 .timeBlock").val(localStorage.getItem("11"));
+$("#hour12 .timeBlock").val(localStorage.getItem("12"));
+$("#hour01 .timeBlock").val(localStorage.getItem("01"));
+$("#hour02 .timeBlock").val(localStorage.getItem("02"));
+$("#hour03 .timeBlock").val(localStorage.getItem("05"));
+$("#hour04 .timeBlock").val(localStorage.getItem("04"));
+$("#hour05 .timeBlock").val(localStorage.getItem("08"));
 
 
