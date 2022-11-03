973d4f8e0d8da4e31bb1fde1d5761e5a1903d345
aniecres <111096788+aniecres@users.noreply.github.com>
2022-11-02 21:25:25 -0400

    css - fixing timeblocks and locks

--- Develop/script.js
+++ Develop/script.js
@@ -2,11 +2,14 @@
 // const date = new Date;
 // console.log(date);
 
-let current = moment().format("dddd, MMMM, YYYY");
+let current = moment().format("dddd, MMMM YYYY");
 let newDate = document.getElementById("currentDay");
 newDate.innerHTML = current;
+let currentDate = moment().format("HH");
 
 
 // Blocks for the past, present, future
-
+$(".time-div").each(function() {
+    let timeDiv = $(this).attr("id").split("-")[1];
+})
 
