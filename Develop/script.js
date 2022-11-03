e98412673ff77ecddbc1341260a2339b2a464d18
aniecres <111096788+aniecres@users.noreply.github.com>
2022-11-02 22:52:06 -0400

    update js

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,27 @@
-// adding current day
-// const date = new Date;
-// console.log(date);
 
+// adding current day
 let current = moment().format("dddd, MMMM YYYY");
 let newDate = document.getElementById("currentDay");
 newDate.innerHTML = current;
-let currentDate = moment().format("HH");
+
 
 
 // Blocks for the past, present, future
-$(".time-div").each(function() {
-    let timeDiv = $(this).attr("id").split("-")[1];
-})
+let currentDate = moment().format("HH");
 
+$(".time").each(function() {
+    let time = $(this).attr("id").split("-")[1];
+
+
+if (currentDate == time) {
+    $(this).addClass("present");
+    $(this).children(".time-block").addClass("present");
+}  else if (currentDate < time) {
+    $(this).removeClass("present");
+    $(this).addClass("future");
+}  else if (currentDate > time) {
+    $(this).removeClass("future");
+    $(this).addClass("past");
+}
+
+})
